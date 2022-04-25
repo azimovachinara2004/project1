747c45b67f9d74d9b417d8a883322ba6b3f8316a
Chinara <azimovachinara2004@gmail.com>
2022-04-25 21:34:16 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import data from './helper/data';
 import './App.css';
+//import Loading from './components/Loading';
+import {SpinnerInfinity} from 'spinners-react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[userData,setUserData]=useState(data)
+  const[isLoading,setIsLoading]=useState(true)
+
+  setTimeout(()=>{
+  setIsLoading(false)
+  },2000)
+   return (
+    <div>
+    <h1> Marvel Heroes</h1>
+   
+    {isLoading ? (
+        <SpinnerInfinity
+          className="spinner"
+          size={100}
+          thickness={172}
+          speed={100}
+         // color="rgba(172, 57, 100, 0.85)"
+         color="#1c2b75"
+        />
+      ) : (
+        <div className="container">
+     {
+       userData.map((data)=>{
+         return(
+           <div className='cards'>
+               <img src={data.img}/>
+           <h3>{data.name}</h3>
+           <h4>{data.film_name}</h4>
+           <p>{data.appearance}</p>
+                    </div>
+           
+         )
+       })
+     }
+      </div>
+      )}
+      
+      </div>
   );
 }
 
